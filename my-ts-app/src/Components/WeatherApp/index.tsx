4779980dcf20337c0950d077b50ad351a409e88f
javad yousefi <javad.yousefi373@gmail.com>
2024-02-15 17:31:11 +0330

    sending request to weather api

--- my-ts-app/src/Components/WeatherApp/index.tsx
+++ my-ts-app/src/Components/WeatherApp/index.tsx
@@ -1,13 +1,17 @@
 import WAInput from "../Common/WAInput";
+import WAButton from "../Common/WAButton";
 import { useState } from "react";
+import { axiosTest } from "../../Core/Services/api/Api";
 
 const index = () => {
 
   const [city,setCity]=useState("");
+  const [w,setw]=useState("");
 
   return (
     <div>
         <WAInput handleChange={(event)=>{setCity(event.target.value)}} name="hi"/>
+       <WAButton handleClick={()=>axiosTest()} />
         {city}
     </div>
   )
--- my-ts-app/src/Core/Services/api/Api.tsx
+++ my-ts-app/src/Core/Services/api/Api.tsx
@@ -1,1 +1,20 @@
 import axios from "axios";
+// type cityWeather = {
+//   name: string;
+//   local_names: {};
+//   lat: Number;
+//   lon: Number;
+//   country: string;
+// };
+async function axiosTest() {
+    try {
+      const response = await axios.get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=tehran,&appid=5e06c9af8e2ce3776ccc77d2e8e48766`
+      );
+      console.log(response.data); // Log the response data
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+export { axiosTest };
