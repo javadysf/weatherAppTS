5e59ec6283e5b5cd9b794c1b81918f14ce1d4a17
javad yousefi <javad.yousefi373@gmail.com>
2024-02-18 00:07:45 +0330

    sending request to weather api 2

--- my-ts-app/src/Components/WeatherApp/index.tsx
+++ my-ts-app/src/Components/WeatherApp/index.tsx
@@ -1,13 +1,37 @@
 import WAInput from "../Common/WAInput";
 import WAButton from "../Common/WAButton";
-import { useState } from "react";
+import { useState }  from "react";
+import React from "react";
 import { axiosTest } from "../../Core/Services/api/Api";
+// interface cityWeather  {
+//   name: string;
+//   local_names: {};
+//   lat: Number;
+//   lon: Number;
+//   country: string;
+// };
+
 
 const index = () => {
 
-  const [city,setCity]=useState("");
-  const [w,setw]=useState("");
+  const getData = async() =>{
+         const data:Array = await axiosTest();
+         setw(data)
+         console.log(data);
+         
+         
+  }
 
+  const [city,setCity]=useState("");
+  const [w,setw]=React.useState<
+  Array<{
+    name: string;
+      local_names: {};
+      lat: Number;
+      lon: Number;
+      country: string;
+  }>
+>([])
   return (
     <div>
         <WAInput handleChange={(event)=>{setCity(event.target.value)}} name="hi"/>
--- my-ts-app/src/Core/Services/api/Api.tsx
+++ my-ts-app/src/Core/Services/api/Api.tsx
@@ -7,14 +7,32 @@ import axios from "axios";
 //   country: string;
 // };
 async function axiosTest() {
-    try {
-      const response = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=tehran,&appid=5e06c9af8e2ce3776ccc77d2e8e48766`
-      );
-      console.log(response.data); // Log the response data
-      return response.data;
-    } catch (error) {
-      console.log(error);
-    }
+  
+  // const api = axios.create({
+  //   baseURL: 'http://api.openweathermap.org/geo/1.0/direct?q=tehran,&appid=5e06c9af8e2ce3776ccc77d2e8e48766', // آدرس API مورد نظر
+    
+  //   // headers: {
+  //   //   Authorization: 'Bearer 5e06c9af8e2ce3776ccc77d2e8e48766', // توکن دریافتی
+  //   // },
+  // });
+  axios.get("http://api.openweathermap.org/geo/1.0/direct?q=tehran,&appid=5e06c9af8e2ce3776ccc77d2e8e48766",{ headers: { 'Content-Type': 'application/json'}})
+  .then(response => {
+    console.log(response.data);
+    
+    return (response.data);
+  })
+  .catch(error => {
+    console.error('خطا در دریافت اطلاعات:', error);
+  });
+
+    // try {
+    //   const response = await axios.get(
+    //     `http://api.openweathermap.org/geo/1.0/direct?q=tehran,&appid=5e06c9af8e2ce3776ccc77d2e8e48766`
+    //   );
+    //   console.log(response.data); // Log the response data
+    //   return response.data;
+    // } catch (error) {
+    //   console.log(error);
+    // }
   }
 export { axiosTest };
